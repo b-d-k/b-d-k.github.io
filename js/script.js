af0d64d392e2aee91423cc468fecb01751e40867
Daniel Krone <mail@supergnu.com>
2016-03-02 10:10:35 -0500

    wrapped js in closure

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
-
-window.onload = init;
-
-
-function init(){
-  alternateBG('proj-row', '#f2f2f2');
-}
+(function(){
+  
+  'use strict';
 
 
-// -- Set BG color on every 2nd element found --
-function alternateBG(el, bgColor){
-
-  var elems = document.getElementsByClassName(el);
-
-  for (var i=elems.length - 1; i >= 0; i--) {
-    if( i%2!==0){
-      elems[i].style.backgroundColor = bgColor;
-    }
-  };
+  window.onload = init;
 
 
-}
+  function init(){
+    alternateBG('proj-row', '#f2f2f2');
+  }
+
+
+  // -- Set BG color on every 2nd element found --
+  function alternateBG(el, bgColor){
+
+    var elems = document.getElementsByClassName(el);
+
+    for (var i=elems.length - 1; i >= 0; i--) {
+      if( i%2!==0){
+        elems[i].style.backgroundColor = bgColor;
+      }
+    };
+
+
+  }
+
+})();
